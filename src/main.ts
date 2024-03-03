dcb9733fa2b0d88a928e850d2fa87a2273b2eaaf
piggaycheng <piggaycheng123@gmail.com>
2024-03-04 00:27:55 +0800

    change example config

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { Polar } from "./index"
 const svgEl = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 document.querySelector("body")!.append(svgEl);
 const polar = new Polar("svg", {
-  width: 300,
-  height: 200,
+  width: 500,
+  height: 500,
   angleAxis: {
     startAngle: Math.PI * 1.5,
     endAngle: Math.PI * 2,
     scaleWeight: [2, 2, 2, 2, 2, 1, 1, 1, 1, 1]
-  }
+  },
+  data: [20, 40, 60]
 });

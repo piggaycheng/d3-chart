3da7821c8afb7d38438d17fcce56c80d38561e2e
Yu Cheng <yucheng@mirle.com.tw>
2024-03-04 11:41:48 +0800

    complete ticks

--- src/charts/polar.ts
+++ src/charts/polar.ts
@@ -4,6 +4,7 @@ import * as _ from "lodash-es"
 type Config = {
   width?: string | number;
   height?: string | number;
+  padding?: number;
   angleAxis?: AngleAxis;
   radiusAxis?: RadiusAxis;
   data: number[];
@@ -29,6 +30,7 @@ class Polar {
   defaultConfig: Config = {
     width: 300,
     height: 150,
+    padding: 25,
     angleAxis: {
       startAngle: 0,
       endAngle: Math.PI,
@@ -70,12 +72,24 @@ class Polar {
       .attr("d", (data: any) => {
         return d3.arc()({
           ...data,
-          outerRadius: this._getRadius(),
+          outerRadius: this._getRadius(config),
           innerRadius: 0
         })
       })
       .attr("fill", "none")
       .attr("stroke", "#000")
+
+    const ticks = this._getTicks(config);
+    this.d3Svg
+      .append("g")
+      .selectAll("text")
+      .data(ticks)
+      .enter()
+      .append("text")
+      .text((tick) => tick.text)
+      .attr("x", (tick) => tick.x)
+      .attr("y", (tick) => tick.y)
+      .attr("text-anchor", "end")
   }
 
   _getAngleAxis(config: Config) {
@@ -86,9 +100,28 @@ class Polar {
     return pie(config.angleAxis!.scaleWeight!)
   }
 
+  _getTicks(config: Config) {
+    const radius = this._getRadius(config);
+    const angleAxis = this._getAngleAxis(config);
+    const svgCenter = this._getCenter();
+
+    const xArray = angleAxis.map((item) => svgCenter[0] + radius * Math.cos(item.endAngle - Math.PI * 0.5));
+    const yArray = angleAxis.map((item) => svgCenter[1] - radius * Math.sin(item.endAngle + Math.PI * 0.5));
+
+    const range = config.angleAxis!.maxValue! - config.angleAxis!.minValue!;
+    const rangePerTick = range / config.angleAxis!.scaleWeight!.length;
+    return config.angleAxis!.scaleWeight!.map((item, index) => {
+      return {
+        text: (index + 1) * rangePerTick,
+        x: xArray[index],
+        y: yArray[index],
+      }
+    })
+  }
+
   _initRadiusAxis(config: Config) {
     const bandScale = d3.scaleBand()
-      .range([0, this._getRadius()])
+      .range([0, this._getRadius(config)])
       .domain(config.radiusAxis!.categories!)
       .padding(config.radiusAxis!.padding!);
     if (config.radiusAxis?.innerPadding) bandScale.paddingInner(config.radiusAxis?.innerPadding);
@@ -96,7 +129,7 @@ class Polar {
 
     this.d3Svg
       .append("g")
-      .attr("transform", `translate(${this._getCenter()[0] - this._getRadius()}, ${this._getCenter()[1]})`)
+      .attr("transform", `translate(${this._getCenter()[0] - this._getRadius(config)}, ${this._getCenter()[1]})`)
       .call(d3.axisBottom(bandScale));
   }
 
@@ -115,7 +148,7 @@ class Polar {
 
     const linearScale = d3.scaleLinear().domain(domain).range(range);
     const bandScale = d3.scaleBand()
-      .range([0, this._getRadius()])
+      .range([0, this._getRadius(config)])
       .domain(config.radiusAxis!.categories!)
       .padding(config.radiusAxis!.padding!);
     this.d3Svg
@@ -129,8 +162,8 @@ class Polar {
         return d3.arc()({
           startAngle: config.angleAxis!.startAngle!,
           endAngle: linearScale(config.data[index])!,
-          outerRadius: this._getRadius() - bandScale(data)!,
-          innerRadius: this._getRadius() - bandScale(data)! - bandScale.bandwidth()
+          outerRadius: this._getRadius(config) - bandScale(data)!,
+          innerRadius: this._getRadius(config) - bandScale(data)! - bandScale.bandwidth()
         })
       })
       .attr("fill", `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`)
@@ -140,8 +173,9 @@ class Polar {
     return [this.d3Svg.node().clientWidth / 2, this.d3Svg.node().clientHeight / 2]
   }
 
-  _getRadius() {
-    return Math.min(this.d3Svg.node().clientWidth / 2, this.d3Svg.node().clientHeight / 2)
+  _getRadius(config: Config) {
+    const padding = config.padding!
+    return Math.min(this.d3Svg.node().clientWidth / 2, this.d3Svg.node().clientHeight / 2) - padding
   }
 }
 

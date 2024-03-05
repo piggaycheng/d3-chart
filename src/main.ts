0c24a7da516844d24e57784def4a5dfc57c9d667
Yu Cheng <yucheng@mirle.com.tw>
2024-03-05 17:35:46 +0800

    change example

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import { Polar } from "./index"
 
+const container = document.createElement("div")
 const svgEl = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-document.querySelector("body")!.append(svgEl);
+container.append(svgEl)
+document.querySelector("body")!.append(container);
 const polar = new Polar("svg", {
   width: 500,
   height: 500,
   angleAxis: {
     startAngle: Math.PI * 1.5,
     endAngle: Math.PI * 2,
-    scaleWeight: [2, 2, 2, 2, 2, 1, 1, 1, 1, 1]
+    scaleWeight: [2, 2, 2, 2, 2, 1, 1, 1, 1, 1],
+    tick: {
+      distance: 15
+    }
+  },
+  radiusAxis: {
+    click: (e) => {
+      console.log(e)
+    },
+    categories: ["A", "B", "C"]
   },
   data: {
     dataset: [20, 40, 60],

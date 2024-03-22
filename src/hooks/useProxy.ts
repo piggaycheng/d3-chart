e2dd2cd50f35da2c3d2f5f34ec15b60043f87fe2
piggaycheng <piggaycheng123@gmail.com>
2024-03-23 00:02:37 +0800

    fix Proxy

--- src/hooks/useProxy.ts
+++ src/hooks/useProxy.ts
@@ -3,7 +3,7 @@ export type Target = Record<string, any>;
 export type SetterCallback = (target: Target, prop: string, value: any, receiver: any, parentProp?: string) => void
 
 export default function () {
-  function createProxy<T extends Target>(target: T, setterCallback: SetterCallback, parentProp?: string, context?: any) {
+  function createProxy<T extends Target>(target: T, setterCallback?: SetterCallback, parentProp?: string, context?: any) {
     const handler = {
       get(target: T, prop: string, receiver: T) {
         if (typeof target[prop] === 'object' && target[prop] !== null) {
@@ -14,7 +14,7 @@ export default function () {
       },
       set(target: T, prop: string, value: any, receiver: T) {
         Reflect.set.apply(null, arguments);
-        setterCallback.apply(context, [...arguments, parentProp]);
+        setterCallback?.apply(context, [...arguments, parentProp]);
         return true
       }
     }

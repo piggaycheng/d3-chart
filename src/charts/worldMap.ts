6cc695c105c961ffb13a28ea5c46470512af258d
piggaycheng <piggaycheng123@gmail.com>
2024-03-23 00:41:51 +0800

    refactor _initWorldMap

--- src/charts/worldMap.ts
+++ src/charts/worldMap.ts
@@ -11,7 +11,7 @@ type Config = WorldMapType.Config
 class WorldMap {
   private _svgSelection: d3.Selection<any, unknown, null, undefined>;
   private _projection: d3.GeoProjection;
-  private _path: d3.GeoPath<any, d3.GeoPermissibleObjects>;
+  private _geoPath: d3.GeoPath<any, d3.GeoPermissibleObjects>;
   private _mapSelection: d3.Selection<d3.BaseType, unknown, null, undefined>;
   private _defaultConfig: Config = {
     type: "worldMap",
@@ -24,6 +24,7 @@ class WorldMap {
   private _finalConfig: Config;
   private _lastConfig?: Config;
   private _worldMapHook: WorldMapType.WorldMapHook;
+  private _countries: WorldMapType.Countries;
 
   constructor(el: string | d3.BaseType, config: Config) {
     const finalConfig = config ? useConfig().mergeConfig<Config>(this._defaultConfig, config) : this._defaultConfig;
@@ -36,6 +37,7 @@ class WorldMap {
       .attr("height", config.height!)
     this._svgSelection
       .append("g").attr("class", "countries")
+    this._svgSelection
       .append("g").attr("class", "linePath")
 
     this._initWorldMap(config)
@@ -43,10 +45,18 @@ class WorldMap {
 
   private async _initWorldMap(config: Config) {
     const { countries, borders, land } = await this._worldMapHook.initGeoData();
+    this._countries = countries;
+
     const projection = d3.geoAzimuthalEqualArea().fitExtent([[0, 0], [config.width!, config.width!]], { type: "Sphere" });
-    const geoPath = d3.geoPath(projection);
-    this._worldMapHook.renderSphere(this._svgSelection, geoPath);
-    this._worldMapHook.renderCountries(this._svgSelection, geoPath, countries);
+    this._projection = projection;
+    this._geoPath = d3.geoPath(projection);
+
+    this._renderWorldMap();
+  }
+
+  private _renderWorldMap() {
+    this._worldMapHook.renderSphere(this._svgSelection, this._geoPath);
+    this._worldMapHook.renderCountries(this._svgSelection, this._geoPath, this._countries);
   }
 
   private _configUpdated(target: Target, prop: string, value: any, receiver: any, parentProp?: string) {
@@ -63,6 +73,11 @@ class WorldMap {
     const temp: Record<string, any> = {}
     temp[key] = value;
     useConfig().mergeConfig<Config>(this._finalConfig, unflatten(temp))
+  }
+
+  rotate(angle: number) {
+    this._projection.rotate([angle, 0, 0])
+    this._renderWorldMap()
   }
 }
 
